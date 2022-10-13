f6b23f919f2d8fa7473ed66f0af92e434f7862d8
br1lisboa <brlisbo@gmail.com>
2022-10-12 22:51:53 -0300

    validaciones en ruta login

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,10 +1,46 @@
 const { response } = require('express')
+const bcryptjs = require('bcryptjs')
+const User = require('../models/user')
 
-const loginController = (req, res = response) => {
+const loginController = async (req, res = response) => {
 
-    res.json({
-        msg: 'Login ok'
-    })
+    const { mail, password } = req.body
+
+    try {
+        // Verificar si el mail existe
+        const users = await User.findOne({ mail })
+        if (!users) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - correo'
+            })
+        }
+
+        // Verificar si el usuario esta activo
+        if (!users.state) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - usuario'
+            })
+        }
+
+        // Verificar password
+        const validPass = bcryptjs.compareSync(password, users.password)
+        if (!validPass) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - password'
+            })
+        }
+
+        // Generar el JWT jason web token
+        res.json({
+            msg: 'Login ok'
+        })
+    } catch (error) {
+        console.log(error)
+        // Status 500 == internal server error
+        res.status(500).json({
+            msg: 'Ups! Algo salio mal. Hable con el administrador'
+        })
+    }
 
 }
 
