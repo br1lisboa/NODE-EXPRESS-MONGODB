453f8414f778929a454ab825830852ad6fbc86cc
br1lisboa <brlisbo@gmail.com>
2022-10-20 10:31:35 -0300

    terminada busqueda de usarios flexible, con expresion regular modificada, y utilizando paint de mongo

--- controllers/search.controllers.js
+++ controllers/search.controllers.js
@@ -1,11 +1,77 @@
 const { response, request } = require("express");
+const User = require("../models/user");
+
+// > Mongo nos cubre con algunas validaciones
+const { ObjectId } = require('mongoose').Types
+
+const allowedSchemas = [
+    'categories',
+    'products',
+    'roles',
+    'users'
+]
+
+const userSearch = async (term = '', res = response) => {
+
+    const isMongoID = ObjectId.isValid(term) // Retrona TRUE o FALSE dependiendo si es o no un ID de MongoDB
+    //console.log(isMongoID)
+    //console.log(term)
+
+    if (isMongoID) {
+
+        const userFind = await User.findById(term)
+        //console.log(userFind)
+        return res.status(200).json({
+            results: (userFind) ? [userFind] : []
+        })
+    }
+
+    // Con esta expresion irregular hacemos insensible a las keysSensitive al term
+    const regex = new RegExp(term, 'i')
+
+    const users = await User.find({
+        // Esto es propio de mongo, presionar $ y se habilitan las opciones or, and, etc..
+        // Aca le digo que haga match con name O mail
+        $or: [{ name: regex }, { mail: regex }],
+        $and: [{ state: true }]
+    })
+    res.json({
+        results: users
+    })
+
+}
 
 
 const search = (req = request, res = response) => {
 
-    res.json({
-        msg: 'buscar...'
-    })
+    const { schema, term } = req.params
+    //console.log(schema, term)
+
+    if (!allowedSchemas.includes(schema)) {
+        return res.status(400).json({
+            msg: `Las colecciones permitidas son: ${allowedSchemas}`
+        })
+    }
+
+    switch (schema) {
+        case 'categories':
+
+            break;
+
+        case 'products':
+
+            break;
+
+        case 'users':
+            userSearch(term, res)
+            break;
+
+        default:
+            res.status(500).json({
+                msg: 'Se le olvido hacer esta busqueda'
+            })
+    }
+
 }
 
 
